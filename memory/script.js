98b94f1aa790de94e68489afd6dc946462c61387
Bianca Amberger <bianca@amberger.at>
2026-03-28 14:37:46 +0100

    added Highscore, Design

--- memory/script.js
+++ memory/script.js
@@ -1,5 +1,14 @@
 
-let allCards = ['🤑', '🤑', '🤡', '🤡', '😴', '😴', '🥶', '🥶', '👿', '👿', '💀', '💀', '🎃', '🎃', '🤢', '🤢'];
+let allCards = [
+    'fa-php','fa-php',
+    'fa-github','fa-github',
+    'fa-java','fa-java',
+    'fa-python','fa-python',
+    'fa-docker','fa-docker',
+    'fa-wordpress','fa-wordpress',
+    'fa-jedi-order','fa-jedi-order',
+    'fa-stack-overflow','fa-stack-overflow',
+];
 let flippedCards = [];
 
 //DIFFICULTY
@@ -21,6 +30,10 @@ document.getElementById("difficulty").addEventListener("click", function(event){
     button.classList.add("btn-primary");
 
     difficulty = difficulties[button.id];
+
+    let highscoreKey = "memoryHighscore_" + difficulty;
+    highscore = localStorage.getItem(highscoreKey) || 0;
+    document.getElementById("highscoreMessage").textContent = "Highscore: " + highscore;
 
     setupGame();
 });
@@ -51,22 +64,27 @@ function setupGame(){
 
 //MESSAGES und TRACKER
 let winningMessage = document.getElementById("winningMessage");
-let matchMessage = document.getElementById("matchMessage");
+//let matchMessage = document.getElementById("matchMessage");
 let matchesMessage = document.getElementById("matchesMessage");
 let moveMessage = document.getElementById("moveMessage");
+let highscoreKey = "memoryHighscore_" + difficulty;
+let highscore = localStorage.getItem(highscoreKey) || 0;
+let highscoreMessage = document.getElementById("highscoreMessage");
+highscoreMessage.textContent = "Highscore: " + highscore;
 
 let checkMatchProcess = 0;
 let moveTracker = 0;
 let matchCounter = 0;
 
+//KARTE WIRD UMGEDREHT
 function flipCard(event) {
     if(checkMatchProcess == 0) {
-        const card = event.target;
+        const card = event.currentTarget;
         if(card.classList.contains('flipped') || card.classList.contains('matched')) return; //checken ob eh nicht 2 mal die gleiche karte
 
-        matchMessage.textContent = "";
+        //matchMessage.textContent = "";
         card.classList.add('flipped');
-        card.textContent = card.dataset.symbol;
+        card.innerHTML = `<i class="fa-brands ${card.dataset.symbol}"></i>`;
         flippedCards.push(card);
 
         if(flippedCards.length >= 2) {
@@ -82,7 +100,7 @@ function checkMatch(){
     moveMessage.textContent = moveTracker + " Moves";
 
     if(flippedCards[0].dataset.symbol === flippedCards[1].dataset.symbol){
-        matchMessage.textContent = "It's a Match!";
+        //matchMessage.textContent = "It's a Match!";
         flippedCards[0].classList.remove('flipped');
         flippedCards[0].classList.add('matched');
         flippedCards[1].classList.remove('flipped');
@@ -96,6 +114,14 @@ function checkMatch(){
         if(matchCounter >= cards.length / 2) {
             winningMessage.style.display = "block";
             document.getElementById("moveMessageFinal").textContent = "You won in " + moveTracker + " moves!";
+            
+            let highscoreKey = "memoryHighscore_" + difficulty;
+
+            if(highscore == 0 || moveTracker < highscore){
+                highscore = moveTracker;
+                localStorage.setItem(highscoreKey, highscore);
+            }
+            highscoreMessage.textContent = "Highscore: " + highscore;
         }
     } else {
         setTimeout(undoClicked, 800);
@@ -106,8 +132,8 @@ function checkMatch(){
 function undoClicked() {
     flippedCards[0].classList.remove('flipped');
     flippedCards[1].classList.remove('flipped');
-    flippedCards[0].textContent = "";
-    flippedCards[1].textContent = "";
+    flippedCards[0].innerHTML = "";
+    flippedCards[1].innerHTML = "";
     undoCards();
 }
 
@@ -128,7 +154,7 @@ function resetGame() {
 
     moveMessage.textContent = moveTracker + " Moves";
     matchesMessage.textContent = matchCounter + " Matches";
-    matchMessage.textContent = "";
+    //matchMessage.textContent = "";
     winningMessage.style.display = "none";
 
     const allCards = document.querySelectorAll('.card');
